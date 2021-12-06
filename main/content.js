23177e019d7ac0902cdb7b699b0c65c76e20807c
Mitr <MITRAMANSOURI78@GMAIL.COM>
2021-12-06 21:08:34 +0330

    commit

--- main/content.js
+++ main/content.js
@@ -60,7 +60,35 @@ function stopTheCode(){
     chrome.runtime.sendMessage({todo: "btn_is_pushed"});
     sendButtonMssg2();
 }
+/*
+//hopefully it will be pushed
 
+        var node = document.getElementsByTagName("body")[0].getElementsByClassName("notebook-vertical")[0]
+                        .getElementsByClassName("notebook-horizontal")[0].getElementsByClassName("layout vertical grow")[0]
+                        .getElementsByClassName("layout horizontal grow")[0].getElementsByClassName("layout vertical grow notebook-tab-content")[0]
+                        .getElementsByClassName("overflow-flexbox-workaround")[0].getElementsByClassName("notebook-container")[0]
+                        .getElementsByClassName("notebook-scrolling-horizontal")[0].getElementsByClassName("notebook-content-background")[0]
+                        .getElementsByClassName("notebook-content ")[0].getElementsByClassName("notebook-cell-list")[0]
+                        .getElementsByClassName("cell code icon-scrolling")[0].getElementsByClassName("main-content")[0]
+                        .getElementsByClassName("codecell-input-output")[0].getElementsByClassName("inputarea horizontal layout code")[0]
+                        .getElementsByClassName("editor flex lazy-editor")[0].getElementsByClassName("editor flex monaco")[0]
+                        .getElementsByClassName("monaco-editor no-user-select showUnused showDeprecated vs")[0].getElementsByClassName("overflow-guard")[0]
+                        .getElementsByClassName("monaco-scrollable-element editor-scrollable vs")[0].getElementsByClassName("lines-content monaco-editor-background")[0]
+                        .getElementsByClassName("view-lines monaco-mouse-cursor-text")[0].getElementsByClassName("view-line")[0];
+
+        console.log(node)
+        var text = document.getElementsByTagName("body")[0].getElementsByClassName("notebook-vertical")[0]
+                        .getElementsByClassName("notebook-horizontal")[0].getElementsByClassName("layout vertical grow")[0]
+                        .getElementsByClassName("layout horizontal grow")[0].getElementsByClassName("layout vertical grow notebook-tab-content")[0]
+                        .getElementsByClassName("overflow-flexbox-workaround")[0].getElementsByClassName("notebook-container")[0]
+                        .getElementsByClassName("notebook-scrolling-horizontal")[0].getElementsByClassName("notebook-content-background")[0]
+                        .getElementsByClassName("notebook-content ")[0].getElementsByClassName("notebook-cell-list")[0]
+                        .getElementsByClassName("cell code icon-scrolling")[0].getElementsByClassName("main-content")[0]
+                        .getElementsByClassName("codecell-input-output")[0].getElementsByClassName("output")[0].getElementsByClassName("output-content")[0]
+                        .getElementsByClassName("output-iframe-container")[0].getElementsByClassName("output-iframe-sizer").[0]
+                        .getElementsByClassName("execute_result output_text").[0]
+        console.log(text)
+        node.textContent = "<p>This is <strong>my</strong> paragraph.</p>";*/
 var runButton = document.getElementById("run-btn");
 runButton.addEventListener("click", runTheCode);
 
