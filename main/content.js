213950d7096aa0018019bcdd9f9ff04df19c5ab3
zahraajamali <60221929+zahraajamali@users.noreply.github.com>
2021-12-07 18:39:53 +0330

    print solutions function

--- main/content.js
+++ main/content.js
@@ -36,51 +36,51 @@ function runTheCode(){
     }
     console.log(code_text);
 
-    document.getElementById("d").style.height = "350px" //change heigth of output box
-    var solution_text = "found 5 solutions: ....";
-
-    var el = document.createElement("div");   //create a div that contain all results from stackoverflow  
-    el.setAttribute("id", "result");   
-    document.getElementById("output").appendChild(el);    //add this div to div output
-    el.innerHTML += `<div id="d1">` + solution_text + `</div>`;      // print the number of result that found   
-   
-    var el2 = document.createElement("ul");    //create list for slutions
-    el2.setAttribute("id", "list"); 
-    document.getElementById("result").appendChild(el2);
-
-
-    var tx = 
-    `Generally, it means that you are providing an index for which a list element does not exist.
+    var solutions = `Generally, it means that you are providing an index for which a list element does not exist.
     E.g, if your list was [1, 3, 5, 7], and you asked for the element at index
     10, you would be well out of bounds and receive an error, as only elements 0
     through 3 exist.`
-
-    for(i=0 ; i<5 ; i++){    //in this loop create items of list
-        var el3 = document.createElement("li");
-        el3.setAttribute("id", i.toString());              
-        document.getElementById("list").appendChild(el3);         
-        
-        var el4 = document.createElement("div");     // adiv for number of solution
-        el4.setAttribute("id", "df"+i.toString());             
-        var textnode = document.createTextNode("solution "+(i+1)+" :");         
-        el4.appendChild(textnode); 
-        document.getElementById(i.toString()).appendChild(el4);
-
-        var el5 = document.createElement("div");    // a div for show solution
-        el5.setAttribute("id", "ds"+i.toString());             
-        var textnode2 = document.createTextNode(tx);         
-        el5.appendChild(textnode2);
-        document.getElementById(i.toString()).appendChild(el5); 
-
-    
-    }
-    // console.log(solutionArea);
-    // solutionArea.innerHTML += `<div>` + solution_text + `</div>`;
-    // console.log(solutionArea);
-    // output the results in that text box with other outputs
+    printSolutions(solutions);
 
     chrome.runtime.sendMessage({todo: "btn_is_pushed"});
     sendButtonMssg1();
+}
+
+function printSolutions(solutions){
+
+    var numberOfSolution = 5;
+
+    document.getElementById("d").style.height = "350px" //change heigth of output box
+    var start_text = "found 5 solutions: ....";
+
+    var result = document.createElement("div");   //create a div that contain all results from stackoverflow  
+    result.setAttribute("id", "result");   
+    document.getElementById("output").appendChild(result);    //add this div to div output
+    result.innerHTML += `<div id="d1">` + start_text + `</div>`;      // print the number of result that found   
+   
+    var List = document.createElement("ul");    //create list for slutions
+    List.setAttribute("id", "list"); 
+    document.getElementById("result").appendChild(List);
+
+
+    for(i=0 ; i< numberOfSolution ; i++){    //in this loop create items of list
+        var Li = document.createElement("li");
+        Li.setAttribute("id", i.toString());              
+        document.getElementById("list").appendChild(Li);         
+        
+        var solutionNumber = document.createElement("div");     // a div for number of solution
+        solutionNumber.setAttribute("id", "s"+i.toString());             
+        var textnode = document.createTextNode("solution "+(i+1)+" :");         
+        solutionNumber.appendChild(textnode); 
+        document.getElementById(i.toString()).appendChild(solutionNumber);
+
+        var description = document.createElement("div");    // a div for show solution
+        description.setAttribute("id", "d"+i.toString());             
+        textnode = document.createTextNode(solutions);         
+        description.appendChild(textnode);
+        document.getElementById(i.toString()).appendChild(description); 
+   
+    }
 }
 
 
