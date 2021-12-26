104e954756019cd1ca9d2bda4ef44c9d99edc8c0
zahraajamali <60221929+zahraajamali@users.noreply.github.com>
2021-12-26 11:42:47 +0330

    jest test

---
+++ util.test.js
@@ -0,0 +1,45 @@
+const {getInnerCode} = require('./util');
+
+test('should turn solution array to html tags', ()=>{
+    var expected_input = [`The "SyntaxError: unexpected EOF while parsing"`];
+    var expected_output = 
+    `<div id="pycee-inserted" style="padding: 10px; margin: 10px auto; width: 92%">`
+        + `<div id="pycee-head" style="margin: 30px auto; color: #3770A0; font-size: 18px; border-bottom: 1px solid; font-weight: 600;">` 
+            + `<p style="margin-left: 15px; "> Pycee found ` + 1 + " solutions </p>" 
+        + '</div>'
+
+        + `<div id="pycee-solutions" style="margin: 0 10px;">`
+            + `<p style="color: #3770A0; font-size: 14px; font-weight: 600;"> Soultion ` + 1 + ": </p>"
+                                            // here sits the solution
+            + `<p style="margin: 0 10px;">` + `The "SyntaxError: unexpected EOF while parsing"` + "</p>"
+            + '<br>'
+        + "</div>"
+    + '</div>';
+    const text = getInnerCode(expected_input);
+    expect(text).toStrictEqual(expected_output);
+});
+
+
+test('should turn solution array to html tags', ()=>{
+    var expected_input = [`The "SyntaxError: unexpected EOF while parsing"`, "ion array to html tags (2 ms    "];
+    var expected_output = 
+    `<div id="pycee-inserted" style="padding: 10px; margin: 10px auto; width: 92%">`
+        + `<div id="pycee-head" style="margin: 30px auto; color: #3770A0; font-size: 18px; border-bottom: 1px solid; font-weight: 600;">` 
+            + `<p style="margin-left: 15px; "> Pycee found ` + 2 + " solutions </p>" 
+        + '</div>'
+
+        + `<div id="pycee-solutions" style="margin: 0 10px;">`
+            + `<p style="color: #3770A0; font-size: 14px; font-weight: 600;"> Soultion ` + 1 + ": </p>"
+                                            // here sits the solution
+            + `<p style="margin: 0 10px;">` + `The "SyntaxError: unexpected EOF while parsing"` + "</p>"
+            + '<br>'
+
+            + `<p style="color: #3770A0; font-size: 14px; font-weight: 600;"> Soultion ` + 2 + ": </p>"
+            // here sits the solution
+            + `<p style="margin: 0 10px;">` +"ion array to html tags (2 ms    " + "</p>"
+            + `<br>`
+        + "</div>"
+    + '</div>';
+    const text = getInnerCode(expected_input);
+    expect(text).toStrictEqual(expected_output);
+});
