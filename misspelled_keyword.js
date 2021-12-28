05bd6c67aa30a20ed6d413050434ccd59058fae3
zahraajamali <60221929+zahraajamali@users.noreply.github.com>
2021-12-28 15:47:04 +0330

    find misspelled world

--- misspelled_keyword.js
+++ misspelled_keyword.js
@@ -1,7 +1,7 @@
 // kyewords in python
 keywords = [ "False","None","True","and","as","assert","async","await","break","class","continue","def","del","elif","else","except"
     ,"finally","for","from","global","if","import","in","is","lambda","nonlocal","not","or","pass","raise"
-    ,"return","try","while","with","yield"];
+    ,"return","try","while","with","yield" , "print"];
 
 // check not defind error
 function checkError(str)
@@ -14,10 +14,50 @@ function checkError(str)
     return false;
 }
 
-function correctWorld(misspelled_world)
+
+// return the number of difference between missplelled_world and keyword
+function getDifference(a, b)
 {
+    var i = 0;
+    var j = 0;
+    var result = 0;
+
+    while (j < b.length)
+    {
+        if (a[i] != b[j] || i == a.length)
+            result++;
+        else
+            i++;
+        j++;
+    }
+    return result;
+}
+
+
+// fine most simillar keyword to misspelled_world
+function correctWorld(error)
+{
+    var misspelled_world = checkError(error);
+    if(misspelled_world){
+        var result = [];
+        for (let i=0 ; i <36 ; i++){
+            if(getDifference(keywords[i],misspelled_world) <2 ){
+                result.push(keywords[i])
+            }
+        }
+        return result
+    }
+    else{
+        return false;
+    }
     
+
 
 }
 
-console.log(checkError("NameError: name 'prin' is not defined"))
+// console.log(getDifference("hello", "heilu"));
+// console.log("-----------------------------------------------")
+// console.log(checkError("NameError: name 'prin' is not defined"))
+// console.log("-----------------------------------------------")
+// console.log(correctWorld("impo"))
+console.log(correctWorld("NameError: name 'pri' is not defined"))
